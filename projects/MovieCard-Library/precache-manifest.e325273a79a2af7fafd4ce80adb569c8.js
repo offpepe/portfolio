4ad5a6bb58a1193d7b4bbdcd51e6f6e69d4080df
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-06-24 10:47:35 -0400

    Updates

---
+++ projects/MovieCard-Library/precache-manifest.e325273a79a2af7fafd4ce80adb569c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2a904bc176ca7e6ed8046d65ba305f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00dcbc238bb9f60b7f6",
+    "url": "/static/css/main.47979868.chunk.css"
+  },
+  {
+    "revision": "31185e76c85143ec678e",
+    "url": "/static/js/2.e7162c03.chunk.js"
+  },
+  {
+    "revision": "b00dcbc238bb9f60b7f6",
+    "url": "/static/js/main.21914918.chunk.js"
+  },
+  {
+    "revision": "69a2327a548e92dd40e6",
+    "url": "/static/js/runtime-main.a1d0a2f0.js"
+  }
+]);
